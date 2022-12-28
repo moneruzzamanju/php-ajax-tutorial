4bf70e386a0da99aae6f1f9489fb48fc9ad78529
moneruzzamanju <moneruzzamanju@gmail.com>
2022-12-29 00:42:29 +0600

    Show data on Browser By POST Method

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
 
-$data = $_GET;
+$data = $_POST;
 $data['result'] = 'success';
 
 echo json_encode($data, JSON_PRETTY_PRINT);
--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
 function sendData(){
     let xhr = new XMLHttpRequest();
-xhr.open("GET","data.php?name=moneruzzaman&age=40",true);
-xhr.onreadystatechange = function(){
-    if(this.readyState===4 && this.status===200){
-    document.getElementById('result').innerHTML = this.responseText;
+    xhr.open("POST","data.php",true);
+    
+    xhr.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+    );
+
+    xhr.onreadystatechange = function(){
+        if(this.readyState===4 && this.status===200){
+            document.getElementById('result').innerHTML = this.responseText;
 
 
-        let response = JSON.parse(this.responseText);
-        console.log(response.result);
+            let response = JSON.parse(this.responseText);
+            console.log(response.result);
 
 
-    }
+        }
 }
-    xhr.send();
+    xhr.send("name=moneruzzaman&age=40");
 }
 
 
