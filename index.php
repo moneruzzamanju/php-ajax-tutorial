2d7580eee8dcc9485e158c5e769343ee208c9552
moneruzzamanju <moneruzzamanju@gmail.com>
2022-12-28 23:58:35 +0600

    check JS and button click is ok.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>PHP Ajax Tutorial</title>
+
+</head>
+<body>
+    
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 data-bs-trigger">
+                    <h4>Trigger</h4>
+                    <form action="">
+                        <input type="button" id="send" value="Send data">
+                    </form>
+                </div>
+                <div class="col-md-8 data-value">
+                    <pre id="result"></pre>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,5 @@
+// alert('ok');
+//check JS and button click is ok.
+document.getElementById('send').addEventListener('click',function(){
+    console.log('clicked');
+})
