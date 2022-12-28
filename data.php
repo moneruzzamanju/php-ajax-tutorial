4c64c1668a7eb49b4d0ae31b3ee49d20e75fce73
moneruzzamanju <moneruzzamanju@gmail.com>
2022-12-29 00:17:27 +0600

    Send data By GET Method and show data on console

---
+++ data.php
@@ -0,0 +1,2 @@
+<?php
+echo json_encode($_GET, JSON_PRETTY_PRINT);
--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
-// alert('ok');
-//check JS and button click is ok.
+function sendData(){
+    let xhr = new XMLHttpRequest();
+xhr.open("GET","data.php?name=moneruzzaman&age=40",true);
+xhr.onreadystatechange = function(){
+    if(this.readyState===4 && this.status===200){
+        console.log(this.responseText);
+
+    }
+}
+    xhr.send();
+}
+
+
+
 document.getElementById('send').addEventListener('click',function(){
-    console.log('clicked');
+    sendData();
 })
+
